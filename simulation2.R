792ebdfb3a4baa5516a96639540ea8d27c81f466
m-signo <mirko_signorelli@hotmail.it>
2016-06-21 11:08:21 +0200

    Add files via upload

---
+++ simulation2.R
@@ -0,0 +1,25 @@
+# load libraries:
+library(neat)
+library(pROC)
+
+# load data:
+load('data-sim2.Rdata')
+# NB: adj is the adjacency matrix, nodes a vector with node names,
+# setlist is a list specifying the sets of nodes
+# and correct is a vector indicating the ground truth (0 = enrichment, 1 = no enrichment)
+
+# generate network matrix
+net = networkmatrix(adj, nodes, nettype = 'directed')
+
+# compute the test
+test = neat(alist = setlist, blist = setlist, network = net,
+            nodes = nodes, nettype = 'directed', alpha = 0.05)
+test = test[test$A!=test$B,]
+
+# behaviour under H0:
+summary(test[correct==1,])
+# overall behaviour
+table(correct, test$conclusion)
+
+plot.roc(correct, test$pvalue,axes=T, percent=TRUE, print.auc=TRUE,
+         max.auc.polygon=TRUE, max.auc.polygon.col="white")  
